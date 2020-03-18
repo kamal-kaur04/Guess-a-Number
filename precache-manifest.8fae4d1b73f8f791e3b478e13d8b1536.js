ee79e71c7994ac3e51d43045b380b1f639579f7c
kamal-kaur04 <kmlkaur73@gmail.com>
2020-03-18 16:16:23 +0530

    Updates

---
+++ precache-manifest.8fae4d1b73f8f791e3b478e13d8b1536.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a3e52b91ef7c9f3fae4",
+    "url": "/Guess-Number/static/js/app.c00a6880.chunk.js"
+  },
+  {
+    "revision": "a9c47586964e1597bb5b",
+    "url": "/Guess-Number/static/js/runtime~app.5f0b4ca1.js"
+  },
+  {
+    "revision": "0a9a9549d55e8e52bbf4",
+    "url": "/Guess-Number/static/js/2.2e3fc3cc.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/Guess-Number/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/Guess-Number/favicon.ico"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/Guess-Number/expo-service-worker.js"
+  },
+  {
+    "revision": "d73d132d4baea552915ced1bbdbe0583",
+    "url": "/Guess-Number/manifest.json"
+  },
+  {
+    "revision": "ef58ff9a14b1871e5232fada458806e5",
+    "url": "/Guess-Number/apple/icons/icon_180x180.png"
+  },
+  {
+    "revision": "dd0d8974f196283ab21f6bb647882750",
+    "url": "/Guess-Number/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "b370d9dab464db3e849d854262b5d2e5",
+    "url": "/Guess-Number/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "b4c95fe40cc182ee41709b5386a65a60",
+    "url": "/Guess-Number/apple/splash/icon_1125x2436.png"
+  },
+  {
+    "revision": "24057a53b4b8207ac6128f5a1858a41f",
+    "url": "/Guess-Number/apple/splash/icon_1136x640.png"
+  },
+  {
+    "revision": "d8bfb76284287658cab36f271d2a2a4a",
+    "url": "/Guess-Number/apple/splash/icon_1242x2208.png"
+  },
+  {
+    "revision": "46f33d43a6ee160cfb767b7e565d3963",
+    "url": "/Guess-Number/apple/splash/icon_1242x2688.png"
+  },
+  {
+    "revision": "ff41747f4f3bb2f9a29c2ee494815852",
+    "url": "/Guess-Number/apple/splash/icon_1334x750.png"
+  },
+  {
+    "revision": "79a7a4869dc12a14e286c382d496dfd0",
+    "url": "/Guess-Number/apple/splash/icon_1792x828.png"
+  },
+  {
+    "revision": "7e9fc39b1c37a328126323bbe8cd404b",
+    "url": "/Guess-Number/apple/splash/icon_2208x1242.png"
+  },
+  {
+    "revision": "1fd86c39fec0a11f2f6b61d5a7af0f39",
+    "url": "/Guess-Number/apple/splash/icon_2436x1125.png"
+  },
+  {
+    "revision": "dbbf9209e64340c9d1b78823b8f8abf2",
+    "url": "/Guess-Number/apple/splash/icon_2688x1242.png"
+  },
+  {
+    "revision": "df18c0c33e95b50b62f822d65cd9b909",
+    "url": "/Guess-Number/apple/splash/icon_640x1136.png"
+  },
+  {
+    "revision": "5d0bcf1c916780f7a20f047bb6d8a30c",
+    "url": "/Guess-Number/apple/splash/icon_750x1334.png"
+  },
+  {
+    "revision": "4ed2cf28de535595662c61587b07aa56",
+    "url": "/Guess-Number/apple/splash/icon_828x1792.png"
+  },
+  {
+    "revision": "22a27b82b127c892be1d57e4abfed3dc",
+    "url": "/Guess-Number/index.html"
+  },
+  {
+    "revision": "618daefd069e9a21a39c87a90866fe5c",
+    "url": "/Guess-Number/register-service-worker.js"
+  }
+];
